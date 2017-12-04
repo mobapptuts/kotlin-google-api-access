9e2ec2e9062b138511e7215a739e2cfad98cc88c
Nigel Henshaw <nigel@mobapptuts.com>
2017-12-04 20:11:36 +0900

    Implemented check if Google Play Services is installed on device

---
+++ app/src/main/java/com/mobapptuts/googleapiauthenticator/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.mobapptuts.googleapiauthenticator
+
+import android.app.Activity
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        statusButton.setOnClickListener {
+            checkPreConditions()
+        }
+    }
+
+    private val googleApiAvailability by lazy {
+        GoogleApiAvailability.getInstance()
+    }
+
+    private val googleConnectionStatus by lazy {
+        googleApiAvailability.isGooglePlayServicesAvailable(this)
+    }
+
+    companion object {
+        val REQUEST_GOOGLE_PLAY_SERVICES = 1000
+    }
+
+    private fun isGooglePlayServicesAvailable() =
+            googleConnectionStatus == ConnectionResult.SUCCESS
+
+    private fun showGPSErrorDialog() {
+        val dialog = googleApiAvailability.getErrorDialog(
+                this, googleConnectionStatus, REQUEST_GOOGLE_PLAY_SERVICES
+        )
+        dialog.show()
+    }
+
+    private fun acquireGooglePlayServices() {
+        if (googleApiAvailability.isUserResolvableError(googleConnectionStatus))
+            showGPSErrorDialog()
+        else
+            statusButton.text = resources.getString(R.string.gps_failed)
+    }
+
+    private fun checkPreConditions() {
+        if (!isGooglePlayServicesAvailable())
+            acquireGooglePlayServices()
+        else
+            statusButton.text = resources.getString(R.string.gps_avaialable)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQUEST_GOOGLE_PLAY_SERVICES -> {
+                if(resultCode != Activity.RESULT_OK) {
+                    Toast.makeText(this, "Please install Google Play Services", Toast.LENGTH_SHORT).show()
+                } else
+                    checkPreConditions()
+            }
+            else -> {
+                throw IllegalArgumentException("Unrecongnized request code $requestCode")
+            }
+        }
+    }
+}
